159c7e1f96807b28be9d7bef505945f0d3d86f7b
Aleksey-Chernyshev <lenya.chernyshev.2003@mail.ru>
2024-09-16 14:00:14 +0300

    upd dashboard

--- server/controller/dashboard.controller.js
+++ server/controller/dashboard.controller.js
@@ -4,7 +4,6 @@ const db = require('../db')
 class DashboardController{
     async dashboard(req,res){
         try {   
-
             //res.json(req.user)
              const user = await db.query("SELECT user_name FROM users WHERE user_id = $1", [req.user])
              res.json(user.rows[0])
